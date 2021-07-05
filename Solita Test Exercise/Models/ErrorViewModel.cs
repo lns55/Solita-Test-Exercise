213e1bab4bd7ec79e78e35a87a29abc035f31694
lzn747 <lzn747@gmail.com>
2021-07-05 18:12:51 +0300

    Добавьте файлы проекта.

---
+++ Solita Test Exercise/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Solita_Test_Exercise.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
