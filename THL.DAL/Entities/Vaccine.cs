524fb4077dd93f3532f3f7f903447b920448b816
lzn747 <lzn747@gmail.com>
2021-07-06 16:20:51 +0300

    Data Access Layer and Data Models created.

---
+++ THL.DAL/Entities/Vaccine.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace THL.DAL.Entities
+{
+    class Vaccine
+    {
+        public string Id { get; set; }
+
+        public string SourceBottle { get; set; }
+
+        public string Gender { get; set; }
+
+        public DateTime VaccinationDate { get; set; }
+    }
+}
---
+++ THL.DAL/Entities/VaccineOrder.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace THL.DAL.Entities
+{
+    class VaccineOrder
+    {
+        public string Id { get; set; }
+
+        public int OrderNnumber { get; set; }
+
+        public string ResponsiblePerson { get; set; }
+
+        public string HeatlhCareDistrict { get; set; }
+
+        public string Vaccine { get; set; }
+
+        public int Injections { get; set; }
+
+        public DateTime Arrived { get; set; }
+    }
+}
