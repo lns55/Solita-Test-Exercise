5a7a8e30bb9b1c50e4179a3909d119dd417d99b3
lzn747 <lzn747@gmail.com>
2021-07-06 16:48:44 +0300

    Database context is created.

---
+++ THL.DAL/Context/VaccineContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using THL.DAL.Entities;
+
+namespace THL.DAL.Context
+{
+    class VaccineContext : DbContext
+    {
+        public VaccineContext(DbContextOptions<VaccineContext> options)
+            : base(options)
+        {
+            Database.EnsureCreatedAsync();
+        }
+
+        public DbSet<Vaccine> Vaccine { get; set; }
+
+        public DbSet<VaccineOrder> VaccineOrder { get; set; }
+    }
+}
