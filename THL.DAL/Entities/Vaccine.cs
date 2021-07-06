6da39fbd577fc1498ae603de731b2c4ea80efc99
lzn747 <lzn747@gmail.com>
2021-07-06 16:35:08 +0300

    Data Annotations added to data models.

--- THL.DAL/Entities/Vaccine.cs
+++ THL.DAL/Entities/Vaccine.cs
@@ -1,15 +1,20 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace THL.DAL.Entities
 {
     class Vaccine
     {
+        [Required]
         public string Id { get; set; }
 
+        [Required]
         public string SourceBottle { get; set; }
 
+        [Required, StringLength(15)]
         public string Gender { get; set; }
 
+        [Required, DataType(DataType.DateTime)]
         public DateTime VaccinationDate { get; set; }
     }
 }
--- THL.DAL/Entities/VaccineOrder.cs
+++ THL.DAL/Entities/VaccineOrder.cs
@@ -1,21 +1,29 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace THL.DAL.Entities
 {
     class VaccineOrder
     {
+        [Required]
         public string Id { get; set; }
 
+        [Required, Range(1, 10000)]
         public int OrderNnumber { get; set; }
 
+        [Required, StringLength(20)]
         public string ResponsiblePerson { get; set; }
 
+        [Required, StringLength(5)]
         public string HeatlhCareDistrict { get; set; }
 
+        [Required, StringLength(15)]
         public string Vaccine { get; set; }
 
+        [Required, Range(1,10)]
         public int Injections { get; set; }
 
+        [Required, DataType(DataType.DateTime)]
         public DateTime Arrived { get; set; }
     }
 }
